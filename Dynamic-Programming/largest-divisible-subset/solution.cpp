fd335dbd554afdb3267532ee9f2fbbb11a3d6820
Harshita Parsendiya <harshitaparsendiya@gmail.com>
2026-09-09 12:55:22 +0530

    Add solution: Largest Divisible Subset (Dynamic-Programming)

--- Dynamic-Programming/largest-divisible-subset/solution.cpp
+++ Dynamic-Programming/largest-divisible-subset/solution.cpp
@@ -7,7 +7,7 @@ public:
 
         for(int i=1; i<n; i++){
             for(int j=0; j<i; j++){
-                if(( nums[i]%nums[j]==0|| nums[j]%nums[i]==0 ) && dp[j]+1 > dp[i]){
+                if(( nums[i]%nums[j]==0 || nums[j]%nums[i]==0 ) && dp[j]+1 > dp[i]){
                     dp[i] = dp[j] + 1;
                     hash[i] = j;
                 }
@@ -15,7 +15,7 @@ public:
             
         }
         int maxval=dp[0];
-        int maxidx=-1;
+        int maxidx=0;
         for(int i=1; i<n; i++){
             if(dp[i]>maxval){
                 maxval = dp[i];
