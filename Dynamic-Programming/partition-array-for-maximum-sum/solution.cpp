cef501e558c43e7192c90b66c0984184ffb2dca3
Harshita Parsendiya <harshitaparsendiya@gmail.com>
2026-09-13 01:55:48 +0530

    Medium | Time: 3 ms (86.27%) | Memory: 12.5 MB (31.08%)

---
+++ Dynamic-Programming/partition-array-for-maximum-sum/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int f(int i, int k, vector<int>&dp, vector<int>&arr, int n){
+        if(i==n) return 0;
+        if(dp[i] != -1) return dp[i];
+        int maxi = INT_MIN;
+        int len=0;
+        int maxans = INT_MIN;
+        for(int j=i; j<min(i+k, n); j++){
+           len++;
+           maxi = max(maxi, arr[j]);
+           int sum = len * maxi + f(j+1, k, dp, arr, n);
+           maxans = max(maxans, sum);
+        }
+        return dp[i]=maxans;
+    }
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> dp(n, -1);
+
+        return f(0, k, dp, arr, n);
+    }
+};
